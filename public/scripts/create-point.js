04d217c421ffd675d9a4248778e5fddbe8db6ef2
nacano10 <nacano.pokemon@gmail.com>
2020-06-17 18:10:30 -0300

    Dia 5

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -65,6 +65,8 @@ function handleSelectedItem(event) {
 
     const itemId = itemLi.dataset.id
 
+    console.log('ITEM ID: ', itemId)
+
     // verificar se existem itens selecionados, se sim
     // pegar os items selecionados
 
@@ -87,6 +89,8 @@ function handleSelectedItem(event) {
         // se não estiver selecionado, adicionar à seleção
         selectedItems.push(itemId)
     }
+
+    console.log('selectedItems: ', selectedItems)
     // atualizar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems
 }
